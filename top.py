d0c1e2ee2ea6740da08b5fcd7b9a3624a1802110
davkri22 <davkri22@bergen.org>
2019-01-11 09:41:58 -0500

    davids file

--- top.py
+++ top.py
@@ -1,3 +1,38 @@
-from tkinter import *
+from pygame import *
+init()
 
-class Application(Frame):
+win = display.set_mode((500, 500))
+
+display.set_caption("First Game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+    time.delay(100)
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    keys = key.get_pressed()
+
+    if keys[K_LEFT]:
+        x -= vel
+    if keys[K_RIGHT]:
+        x += vel
+    if keys[K_UP]:
+        y -= vel
+    if keys[K_DOWN]:
+        y += vel
+
+    win.fill((0, 0, 0))
+    draw.rect(win, (255, 0, 0), (x, y, width, height))
+    display.update()
+
+
+quit()
