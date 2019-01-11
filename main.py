b8231a21bb2e6779685ea74e8126e5760de3d3b8
davkri22 <davkri22@bergen.org>
2019-01-11 12:21:46 -0500

    davids file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pygame import *
+from pygame_textinput import *
 init()
-
+textinput = TextInput()
 win = display.set_mode((500, 500))
-
-display.set_caption("First Game")
+clock = time.Clock()
+display.set_caption("Battleship")
 
 x = 50
 y = 50
@@ -13,37 +14,16 @@ vel = 5
 
 run = True
 while run:
-    time.delay(100)
-
-    for e in event.get():
-        if e.type == QUIT:
+    win.fill((255, 255, 255))
+    events = pygame.event.get()
+    for event in events:
+        if event.type == QUIT:
             run = False
-
-    keys = key.get_pressed()
-
-    if keys[K_LEFT]:
-        if x - vel >= 0:
-            x -= vel
-        else:
-            x = 0
-    if keys[K_RIGHT]:
-        if x + vel <= 460:
-            x += vel
-        else:
-            x = 460
-    if keys[K_UP]:
-        if y - vel >= 0:
-            y -= vel
-        else:
-            y = 0
-    if keys[K_DOWN]:
-        if y + vel <= 440:
-            y += vel
-        else:
-            y = 440
-
-    win.fill((0, 0, 0))
-    draw.rect(win, (255, 0, 0), (x, y, width, height))
+    textinput.update(events)
+    win.blit(textinput.get_surface(), (10, 10))
     display.update()
+    clock.tick(30)
+    if textinput.update(events):
+        print(textinput.get_text())
 
 quit()
