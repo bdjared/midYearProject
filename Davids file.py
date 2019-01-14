6b4aa52315770b74dc42decdfba1bbc855cb4df9
davkri22 <davkri22@bergen.org>
2019-01-14 12:27:23 -0500

    davids file

--- Davids file.py
+++ Davids file.py
@@ -7,14 +7,23 @@ class Application(Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.bttn_1a = Button(self,).grid(row=0, column=0)
-        self.bttn_1b = Button(self,).grid(row=0, column=1)
-        self.bttn_2a = Button(self,).grid(row=0, column=2)
-        self.bttn_2b = Button(self,).grid(row=0, column=3)
-        self.bttn_3a = Button(self,).grid(row=1, column=0)
-        self.bttn_3b = Button(self,).grid(row=1, column=1)
-        self.bttn_4a = Button(self,).grid(row=1, column=2)
-        self.bttn_4b = Button(self,).grid(row=1, column=3)
+        self.bttn_1a = Button(self)
+        self.bttn_1b = Button(self)
+        self.bttn_2a = Button(self)
+        self.bttn_2b = Button(self)
+        self.bttn_3a = Button(self)
+        self.bttn_3b = Button(self)
+        self.bttn_4a = Button(self)
+        self.bttn_4b = Button(self)
+        self.bttn_1a.grid(row=0, column=0)
+        self.bttn_1b.grid(row=0, column=1)
+        self.bttn_2a.grid(row=0, column=2)
+        self.bttn_2b.grid(row=0, column=3)
+        self.bttn_3a.grid(row=1, column=0)
+        self.bttn_3b.grid(row=1, column=1)
+        self.bttn_4a.grid(row=1, column=2)
+        self.bttn_4b.grid(row=1, column=3)
+
 
 
 root = Tk()
