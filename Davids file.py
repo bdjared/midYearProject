2d9b05843bcb6be3b7b2bf0e2441db54f9be92ae
davkri22 <davkri22@bergen.org>
2019-01-14 12:25:29 -0500

    davids file

--- Davids file.py
+++ Davids file.py
@@ -7,18 +7,18 @@ class Application(Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.bttn_1a = Button(self,).grid()
-        self.bttn_1b = Button(self,).grid()
-        self.bttn_2a = Button(self,).grid()
-        self.bttn_2b = Button(self,).grid()
-        self.bttn_3a = Button(self,).grid()
-        self.bttn_3b = Button(self,).grid()
-        self.bttn_4a = Button(self,).grid()
-        self.bttn_4b = Button(self,).grid()
+        self.bttn_1a = Button(self,).grid(row=0, column=0)
+        self.bttn_1b = Button(self,).grid(row=0, column=1)
+        self.bttn_2a = Button(self,).grid(row=0, column=2)
+        self.bttn_2b = Button(self,).grid(row=0, column=3)
+        self.bttn_3a = Button(self,).grid(row=1, column=0)
+        self.bttn_3b = Button(self,).grid(row=1, column=1)
+        self.bttn_4a = Button(self,).grid(row=1, column=2)
+        self.bttn_4b = Button(self,).grid(row=1, column=3)
 
 
 root = Tk()
-root.title("")
+root.title("hi")
 root.geometry("200x85")
 app = Application(root)
 root.mainloop()
