1d2967f803579d33e14b1ab64a51f997faaafcff
davkri22 <davkri22@bergen.org>
2019-01-15 12:26:54 -0500

    davids stuff

--- Davids file.py
+++ Davids file.py
@@ -10,25 +10,22 @@ class Application(Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.bttn_1a = Button(self).grid(row=0, column=0, padx=10, pady=10, sticky=W)
-        self.bttn_1b = Button(self).grid(row=0, column=2, padx=10, pady=10, sticky=W)
-        self.bttn_2a = Button(self).grid(row=0, column=4, padx=10, pady=10, sticky=W)
-        self.bttn_2b = Button(self).grid(row=0, column=6, padx=10, pady=10, sticky=W)
-        self.bttn_3a = Button(self).grid(row=1, column=0, padx=10, pady=10, sticky=W)
-        self.bttn_3b = Button(self).grid(row=1, column=2, padx=10, pady=10, sticky=W)
-        self.bttn_4a = Button(self).grid(row=1, column=4, padx=10, pady=10, sticky=W)
-        self.bttn_4b = Button(self).grid(row=1, column=6, padx=10, pady=10, sticky=W)
-        self.bttn_5a = Button(self).grid(row=2, column=0, padx=10, pady=10, sticky=W)
-        self.bttn_5b = Button(self).grid(row=2, column=2, padx=10, pady=10, sticky=W)
-        self.bttn_6a = Button(self).grid(row=2, column=4, padx=10, pady=10, sticky=W)
-        self.bttn_6b = Button(self).grid(row=2, column=6, padx=10, pady=10, sticky=W)
-        self.bttn_7a = Button(self).grid(row=3, column=0, padx=10, pady=10, sticky=W)
-        self.bttn_7b = Button(self).grid(row=3, column=2, padx=10, pady=10, sticky=W)
-        self.bttn_8a = Button(self).grid(row=3, column=4, padx=10, pady=10, sticky=W)
-        self.bttn_8b = Button(self).grid(row=3, column=6, padx=10, pady=10, sticky=W)
-
-
-
+        self.bttn_1a = Button(self, image=self.get_image()).grid(row=0, column=0, padx=10, pady=10, sticky=W)
+        self.bttn_1b = Button(self, image=self.get_image()).grid(row=0, column=2, padx=10, pady=10, sticky=W)
+        self.bttn_2a = Button(self, image=self.get_image()).grid(row=0, column=4, padx=10, pady=10, sticky=W)
+        self.bttn_2b = Button(self, image=self.get_image()).grid(row=0, column=6, padx=10, pady=10, sticky=W)
+        self.bttn_3a = Button(self, image=self.get_image()).grid(row=1, column=0, padx=10, pady=10, sticky=W)
+        self.bttn_3b = Button(self, image=self.get_image()).grid(row=1, column=2, padx=10, pady=10, sticky=W)
+        self.bttn_4a = Button(self, image=self.get_image()).grid(row=1, column=4, padx=10, pady=10, sticky=W)
+        self.bttn_4b = Button(self, image=self.get_image()).grid(row=1, column=6, padx=10, pady=10, sticky=W)
+        self.bttn_5a = Button(self, image=self.get_image()).grid(row=2, column=0, padx=10, pady=10, sticky=W)
+        self.bttn_5b = Button(self, image=self.get_image()).grid(row=2, column=2, padx=10, pady=10, sticky=W)
+        self.bttn_6a = Button(self, image=self.get_image()).grid(row=2, column=4, padx=10, pady=10, sticky=W)
+        self.bttn_6b = Button(self, image=self.get_image()).grid(row=2, column=6, padx=10, pady=10, sticky=W)
+        self.bttn_7a = Button(self, image=self.get_image()).grid(row=3, column=0, padx=10, pady=10, sticky=W)
+        self.bttn_7b = Button(self, image=self.get_image()).grid(row=3, column=2, padx=10, pady=10, sticky=W)
+        self.bttn_8a = Button(self, image=self.get_image()).grid(row=3, column=4, padx=10, pady=10, sticky=W)
+        self.bttn_8b = Button(self, image=self.get_image()).grid(row=3, column=6, padx=10, pady=10, sticky=W)
 
     def get_image(self):
         a = random.randint(0,7)
