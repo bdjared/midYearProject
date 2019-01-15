abcb6eb593a2a2e4e950a65aa59f0843bb93eebd
davkri22 <davkri22@bergen.org>
2019-01-15 14:12:09 -0500

    davids stuff

--- Davids file.py
+++ Davids file.py
@@ -5,8 +5,8 @@ import random
 class Application(Frame):
     def __init__(self, master):
         super(Application, self).__init__(master)
-        self.photo_list = ["photo1.gif", "photo2.gif", "photo3.gif", "photo4.gif", "photo5.gif", "photo6.gif", "photo7.gif",
-                           "photo8.gif"]
+        self.photo_list = ["photo1.gif", "photo2.gif", "photo3.gif", "photo4.gif", "photo5.gif", "photo6.gif",
+                           "photo7.gif", "photo8.gif"]
         self.grid()
         self.create_widgets()
 
