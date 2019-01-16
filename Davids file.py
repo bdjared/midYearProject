90b664f862316f3156c6f75841e88661305aa33d
davkri22 <davkri22@bergen.org>
2019-01-16 14:22:41 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Davids file.py

--- Davids file.py
+++ Davids file.py
@@ -78,7 +78,7 @@ class Application(Frame):
         self.bttn_8b = Button(self, image=self.photo8b)
         self.bttn_8b.photo = self.photo8b
         self.bttn_8b.grid(row=3, column=3, padx=10, pady=10, sticky=W)
-        self.master.after(2000,self.flip)
+        self.flip()
 
     def get_image(self):
         a = random.choice(self.photo_list)
@@ -94,62 +94,52 @@ class Application(Frame):
         self.bttn_1b.config(image=photo)
         self.bttn_1b.photo = photo
         self.bttn_1b.grid(row=0, column=1, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_2a.config(image=photo)
         self.bttn_2a.photo = photo
         self.bttn_2a.grid(row=0, column=2, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_2b.config(image=photo)
         self.bttn_2b.photo = photo
         self.bttn_2b.grid(row=0, column=3, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_3a.config(image=photo)
         self.bttn_3a.photo = photo
         self.bttn_3a.grid(row=1, column=0, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_3b.config(image=photo)
         self.bttn_3b.photo = photo
         self.bttn_3b.grid(row=1, column=1, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_4a.config(image=photo)
         self.bttn_4a.photo = photo
         self.bttn_4a.grid(row=1, column=2, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_4b.config(image=photo)
         self.bttn_4b.photo = photo
         self.bttn_4b.grid(row=1, column=3, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_5a.config(image=photo)
         self.bttn_5a.photo = photo
         self.bttn_5a.grid(row=2, column=0, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_5b.config(image=photo)
         self.bttn_5b.photo = photo
         self.bttn_5b.grid(row=2, column=1, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_6a.config(image=photo)
         self.bttn_6a.photo = photo
         self.bttn_6a.grid(row=2, column=2, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_6b.config(image=photo)
         self.bttn_6b.photo = photo
         self.bttn_6b.grid(row=2, column=3, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_7a.config(image=photo)
         self.bttn_7a.photo = photo
         self.bttn_7a.grid(row=3, column=0, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_7b.config(image=photo)
         self.bttn_7b.photo = photo
         self.bttn_7b.grid(row=3, column=1, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_8a.config(image=photo)
         self.bttn_8a.photo = photo
         self.bttn_8a.grid(row=3, column=2, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
         self.bttn_8b.config(image=photo)
         self.bttn_8b.photo = photo
         self.bttn_8b.grid(row=3, column=3, padx=10, pady=10, sticky=W)
+        self.master.after(2000, self.flip)
+
+    def hi(self):
+        self.bttn_1a.config(image=self.photo1a)
 
 
 root = Tk()
