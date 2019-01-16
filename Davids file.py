ed647683a60b3ceaa94c859fba24418b28b6ae9b
davkri22 <davkri22@bergen.org>
2019-01-16 13:17:34 -0500

    davids stuff

--- Davids file.py
+++ Davids file.py
@@ -87,69 +87,69 @@ class Application(Frame):
 
     def flip(self):
         photo = PhotoImage(file = "blank.gif")
-        self.bttn_1a = Button(self, image=photo)
+        self.bttn_1a.config(image=photo, command=self.click_flip())
         self.bttn_1a.photo = photo
         self.bttn_1a.grid(row=0, column=0, padx=10, pady=10, sticky=W)
-        photo = PhotoImage(file="blank.gif")
-        self.bttn_1b = Button(self, image=photo)
+        self.bttn_1b.config(image=photo)
         self.bttn_1b.photo = photo
         self.bttn_1b.grid(row=0, column=1, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_2a = Button(self, image=photo)
+        self.bttn_2a.config(image=photo)
         self.bttn_2a.photo = photo
         self.bttn_2a.grid(row=0, column=2, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_2b = Button(self, image=photo)
+        self.bttn_2b.config(image=photo)
         self.bttn_2b.photo = photo
         self.bttn_2b.grid(row=0, column=3, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_3a = Button(self, image=photo)
+        self.bttn_3a.config(image=photo)
         self.bttn_3a.photo = photo
         self.bttn_3a.grid(row=1, column=0, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_3b = Button(self, image=photo)
+        self.bttn_3b.config(image=photo)
         self.bttn_3b.photo = photo
         self.bttn_3b.grid(row=1, column=1, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_4a = Button(self, image=photo)
+        self.bttn_4a.config(image=photo)
         self.bttn_4a.photo = photo
         self.bttn_4a.grid(row=1, column=2, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_4b = Button(self, image=photo)
+        self.bttn_4b.config(image=photo)
         self.bttn_4b.photo = photo
         self.bttn_4b.grid(row=1, column=3, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_5a = Button(self, image=photo)
+        self.bttn_5a.config(image=photo)
         self.bttn_5a.photo = photo
         self.bttn_5a.grid(row=2, column=0, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_5b = Button(self, image=photo)
+        self.bttn_5b.config(image=photo)
         self.bttn_5b.photo = photo
         self.bttn_5b.grid(row=2, column=1, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_6a = Button(self, image=photo)
+        self.bttn_6a.config(image=photo)
         self.bttn_6a.photo = photo
         self.bttn_6a.grid(row=2, column=2, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_6b = Button(self, image=photo)
+        self.bttn_6b.config(image=photo)
         self.bttn_6b.photo = photo
         self.bttn_6b.grid(row=2, column=3, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_7a = Button(self, image=photo)
+        self.bttn_7a.config(image=photo)
         self.bttn_7a.photo = photo
         self.bttn_7a.grid(row=3, column=0, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_7b = Button(self, image=photo)
+        self.bttn_7b.config(image=photo)
         self.bttn_7b.photo = photo
         self.bttn_7b.grid(row=3, column=1, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_8a = Button(self, image=photo)
+        self.bttn_8a.config(image=photo)
         self.bttn_8a.photo = photo
         self.bttn_8a.grid(row=3, column=2, padx=10, pady=10, sticky=W)
         photo = PhotoImage(file="blank.gif")
-        self.bttn_8b = Button(self, image=photo)
+        self.bttn_8b.config(image=photo)
         self.bttn_8b.photo = photo
         self.bttn_8b.grid(row=3, column=3, padx=10, pady=10, sticky=W)
+
 
 root = Tk()
 root.title("hi")
