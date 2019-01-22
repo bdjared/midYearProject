6b3eea396197cb2fa8690866e93477d74afaa405
Mark-Malysa <marmal22@bergen.org>
2019-01-22 12:20:26 -0500

    Mark's file

--- Davids file.py
+++ Davids file.py
@@ -6,6 +6,12 @@ import top
 class Application(Frame):
     def __init__(self, master):
         super(Application, self).__init__(master)
+        self.t = StringVar()
+        self.t.set("00:00:00")
+        self.lb = Label(self, textvariable=self.t)
+        self.lb.config(font=("Courier 40 bold"))
+        self.lb.grid(row=5, column=0, columnspan=5)
+        self.timeron = False
         self.master = master
         self.photo_list = ["photo1.gif", "photo2.gif", "photo3.gif", "photo4.gif", "photo5.gif", "photo6.gif",
                            "photo7.gif", "photo8.gif", "photo1.gif", "photo2.gif", "photo3.gif", "photo4.gif",
@@ -16,6 +22,7 @@ class Application(Frame):
 
     def start_game(self):
         self.create_widgets()
+        self.timer_time()
 
     def create_widgets(self):
         self.start_bttn.destroy()
@@ -141,6 +148,56 @@ class Application(Frame):
 
             if self.amt_showing == 2:
                 self.master.after(1000, self.flip)
+
+    def timer_time(self):
+        self.master.after(3000, self.start_timer())
+
+    def reset(self):
+        self.timeron = False
+        self.t.set('00:00:00')
+
+    def start_timer(self):
+
+        self.timeron = True
+        self.timer()
+
+    def stop(self):
+        self.timeron = False
+
+    def timer(self):
+        if self.timeron == True:
+            self.d = str(self.t.get())
+            h, m, s = map(int, self.d.split(":"))
+
+            h = int(h)
+            m = int(m)
+            s = int(s)
+            if (s < 59):
+                s += 1
+            elif (s == 59):
+                s = 0
+                if (m < 59):
+                    m += 1
+                elif (m == 59):
+                    h += 1
+            if (h < 10):
+                h = str(0) + str(h)
+            else:
+                h = str(h)
+            if (m < 10):
+                m = str(0) + str(m)
+            else:
+                m = str(m)
+            if (s < 10):
+                s = str(0) + str(s)
+            else:
+                s = str(s)
+            self.d = h + ":" + m + ":" + s
+
+            self.t.set(self.d)
+
+            self.after(930, self.timer)
+
 
 
 root = Tk()
